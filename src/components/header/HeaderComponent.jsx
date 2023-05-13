9e2ddbfa9497895f59f9d35f1cccbaa1410e47ec
IsaiahPaget <113209222+IsaiahPaget@users.noreply.github.com>
2023-05-13 10:44:17 -0700

    ok now I did it

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -11,29 +11,30 @@ function HeaderComponent(props) {
 	}
 
 	return (
-		<nav className='z-20 sticky top-0 w-full flex lg:gap-8 flex-row p-9'>
+		<nav className='z-20 sticky pointer-events-none top-0 w-full flex lg:gap-8 flex-row p-9'>
 			<a
+				role='button'
 				onClick={scrollToTop}
 				tag='cypher'
 				data-value='Isaiah Paget'
-				className='text-2xl mr-auto rounded-sm p-1 bg-gray-light'
-				href='/'
+				className='text-2xl mr-auto rounded-sm p-1 bg-gray-light pointer-events-auto'
 			>
 				Isaiah Paget
 			</a>
 			<a
+				role='button'
 				onClick={scrollToContact}
 				tag='cypher'
 				data-value='Contact'
-				className='text-2xl p-1'
-				href='/'
+				className='text-2xl p-1 pointer-events-auto'
 			>
 				Contact
 			</a>
 			<a
+				role='button'
 				tag='cypher'
 				data-value='Github'
-				className='text-2xl p-1 max-sm:hidden'
+				className='text-2xl p-1 max-sm:hidden pointer-events-auto'
 				href='https://github.com/isaiahpaget'
 			>
 				Github
